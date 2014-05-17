46cfd49d27ab76f36df5d44750b0c4e88852c1cf
Zazi <gir_zippo@hellokitty.com>
2014-05-17 16:40:28 +0200

    Clean for Master
    Signed-off-by: Zazi <gir_zippo@hellokitty.com>

--- cmake/options.cmake
+++
@@ -1,4 +0,0 @@
-option(TOOLS            "Build map/vmap extraction/assembler tools"                   0)
-option(USE_SCRIPTPCH    "Use precompiled headers when compiling scripts"              1)
-option(USE_PCH          "Use precompiled headers when compiling servers"              1)
-option(WITH_WARNINGS    "Show all warnings during compile"                            0)
